1ca1ae4ebc2e6b4ed1fa73a6cb35c42ee18dcd94
Matthew Ssesanga <matthewssesanga@gmail.com>
2025-08-11 17:40:46 +0300

    Ts config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,19 +3,6 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
-  webpack: (config, { isServer }) => {
-    // Exclude backend directory from compilation
-    config.resolve.alias = {
-      ...config.resolve.alias,
-    };
-    
-    config.module.rules.push({
-      test: /\.ts$/,
-      exclude: [/node_modules/, /src\/backend/],
-    });
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
